3c6d2052c47e174540238a5c8add4bea6e05d348
Ztj <ztj1993@gmail.com>
2024-03-25 22:41:17 +0800

    禁用更新

--- lib/core.ps1
+++ lib/core.ps1
@@ -1021,6 +1021,7 @@ function show_app($app, $bucket, $version) {
 }
 
 function is_scoop_outdated() {
+    return $false
     $now = [System.DateTime]::Now
     try {
         $expireHour = (New-TimeSpan (get_config LAST_UPDATE) $now).TotalHours
